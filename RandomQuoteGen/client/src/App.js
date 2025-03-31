00be08fd1005e1f4682c3486011bcfcb633fe201
Sabari Doss R <sabaridossr@Sabaris-MacBook-Air-2.local>
2025-03-31 19:08:43 +0530

    Back-end

--- RandomQuoteGen/client/src/App.js
+++ RandomQuoteGen/client/src/App.js
@@ -3,21 +3,19 @@ import axios from "axios";
 import "./App.css"; 
 
 const App = () => {
-  const [quote, setQuote] = useState("quotes");
+  const [quote, setQuote] = useState("");  
   const [author, setAuthor] = useState("");
   const [darkMode, setDarkMode] = useState(false);
 
-
   const fetchQuote = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/quotes/random");
+      const res = await axios.get("http://localhost:5001/quotes/random");
       setQuote(res.data.text);
       setAuthor(res.data.author);
     } catch (error) {
       console.error("Error fetching quote:", error);
     }
   };
-
 
   useEffect(() => {
     fetchQuote();
