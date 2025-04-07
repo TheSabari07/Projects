deb1421cfa6ac93b97ce8827fd83409822d1cee0
Sabari Doss R <sabaridossr@Sabaris-MacBook-Air-2.local>
2025-04-07 19:05:52 +0530

    base code

---
+++ mern-chatbot/client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const App = () => {
+
+  const [backend,setBackend] = useState("");
+
+ useEffect(()=>{
+      axios.get('http://localhost:5002')
+      .then(res => setBackend(res.data))
+      .catch(err => console.error(err));
+ },[]); 
+
+  return (
+    <div>
+       <h1>React Frontend</h1>
+       <p>Backend says: {backend}</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ mern-chatbot/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+  });
+
+const PORT = 5002;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
