2909dc2afcb0b22cc0b011c9d4c0af9ac65419e2
Sabari Doss R <sabursd18@gmail.com>
2025-04-15 13:58:18 +0530

    feat: connect backend to MongoDB Atlas

--- mern-chatbot/server/index.js
+++ mern-chatbot/server/index.js
@@ -3,23 +3,28 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const mongoose = require('mongoose');
+const Message = require('./models/Message.js')
 
-mongoose.connect(process.env.MONGO_URI,{
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
-const app = express();
-app.use(cors());
-app.use(express.json());
 
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
+
 
 app.post('/chat', async (req, res) => {
   const { message } = req.body;
 
   try {
+
     const response = await axios.post(
       'https://api.groq.com/openai/v1/chat/completions',
       {
@@ -32,10 +37,15 @@ app.post('/chat', async (req, res) => {
           'Content-Type': 'application/json',
         },
       }
-    );console.log("Groq raw response:", response.data);
+    );
 
+    const botReply = response.data.choices[0].message.content;
 
-    res.json({ reply: response.data.choices[0].message.content });
+  
+    await Message.create({ sender: "user", text: message });
+    await Message.create({ sender: "bot", text: botReply });
+
+    res.json({ reply: botReply });
   } catch (err) {
     console.error("Groq error:", err.response?.data || err.message);
     res.status(500).json({ error: 'Something went wrong with Groq API.' });
@@ -44,5 +54,5 @@ app.post('/chat', async (req, res) => {
 
 const PORT = 5002;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
