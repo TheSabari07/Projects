af40c4cedf66572a08cc0d337edcf78abcf7e0b5
Sabari Doss R <sabursd18@gmail.com>
2025-05-28 19:52:05 +0530

    feat : added home page

---
+++ VidFetch/client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="app-container">
+            <h1>VidFetch</h1>
+            <P>Download videos from social media using the URL</P>
+
+
+        </div>
+    );
+};
+
+export default Home;
