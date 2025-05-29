fc8cade704f48fa5005c380ed35a5c0038f33caf
Sabari Doss R <sabursd18@gmail.com>
2025-05-29 19:55:01 +0530

    style : add style for home page

--- VidFetch/client/src/pages/Home.jsx
+++ VidFetch/client/src/pages/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import UrlInputForm from '../components/UrlInputForm';
 
 const Home = () => {
-
   return (
-
-    <div className="app-container">
-      <h1>VidFetch</h1>
-      <p>Download videos from social media using the URL</p>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-4">
+      <h1 className="text-4xl font-bold text-gray-800 mb-2">VidFetch</h1>
+      <p className="text-gray-600 mb-6 text-center">
+        Download videos from social media using the URL
+      </p>
       <UrlInputForm />
     </div>
-    
   );
 };
 
