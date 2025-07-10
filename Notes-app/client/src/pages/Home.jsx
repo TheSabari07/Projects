60ff28514e5bb27243e2b16dc5fb1cec1523ad9c
Sabari Doss R <sabursd18@gmail.com>
2025-07-10 23:40:05 +0530

    feat(frontend) : update home page

--- Notes-app/client/src/pages/Home.jsx
+++ Notes-app/client/src/pages/Home.jsx
@@ -1,1 +1,16 @@
+import React, { useState } from 'react';
+import FilterBar from '../components/FilterBar';
 
+const Home = () => {
+  const [notes, setNotes] = useState([]);
+
+  return (
+    <div className="home-container">
+      
+      <FilterBar />
+      
+    </div>
+  );
+};
+
+export default Home;
